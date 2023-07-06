cec66e6ca9c3a7c792223abe8f0ba011f4a127fb
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-06 11:09:55 +0700

    Some transition practices

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-"use strict";
+'use strict';
 console.log(`See The Odin Project lesson's code examples on GitHub branches.
 Each branch is a separate lesson.`);
+
+const div = document.querySelector('div.element');
+
+let a = 0;
