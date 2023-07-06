20f8c40fb5d981ab88a906ed43194b223ad2da3e
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-06 16:29:46 +0700

    Animation appeat from scratch

--- dynamicUI/script.js
+++ dynamicUI/script.js
@@ -7,3 +7,12 @@ const text = `transform: translate(400%, 0rem) rotateZ(11450deg) scale(0.02)`;
 container.addEventListener('click', () => {
   container.classList.toggle('clicked');
 });
+
+const result = document.querySelector('.result');
+
+window.addEventListener('click', (e) => {
+  const div = document.createElement('div');
+  div.classList.add('appear');
+  div.textContent = 'Some message here!';
+  result.appendChild(div);
+});
