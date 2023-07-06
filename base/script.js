5ae62bc7df4d1d318c76ca4f8e32c6c62fb4b735
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-06 12:54:12 +0700

    Reset base folder

--- base/script.js
+++ base/script.js
@@ -1,7 +1,3 @@
 'use strict';
 console.log(`See The Odin Project lesson's code examples on GitHub branches.
 Each branch is a separate lesson.`);
-
-const div = document.querySelector('div.element');
-
-let a = 0;
